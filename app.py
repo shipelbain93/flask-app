9c5e90daeecbc079937e1025babf0332aafaab37
Max_Shipelbain <at_maail@mail.ru>
2025-04-29 11:01:36 +0000

    Update app message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Привет, мир! Это мой первый сайт на Flask!"
+    return "Привет, мир! Это обновленная версия сайта!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
