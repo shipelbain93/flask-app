d409b0f7a87dc6da451e8b98febf29e41709ab9b
Max_Shipelbain <at_maail@mail.ru>
2025-04-29 11:28:21 +0000

    Fix syntax error in app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Привет, мир! Это обновленная версия сайта!"
+def hello():
+    return "Привет, мир! Это мой первый сайт на Flask!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
